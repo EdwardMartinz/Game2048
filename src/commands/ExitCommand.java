bdc39ac982faf18fb0446ed16fbb9d375a3f8631
Edu <36489953+EdwardMartinz@users.noreply.github.com>
2018-03-07 23:22:46 +0100

    Update ExitCommand.java

--- src/commands/ExitCommand.java
+++ src/commands/ExitCommand.java
@@ -5,16 +5,13 @@ import logic.Game;
 
 public class ExitCommand extends NoParamsCommand {
 
-	// Attributes
 	private final static String COMMAND_INFO = "exit";
 	private final static String HELP_INFO = "terminate the program\n";
 
-	// Constructor
 	public ExitCommand() {
 		super(COMMAND_INFO, HELP_INFO);
 	}
 
-	// Execute
 	@Override
 	public boolean execute(Game game) {
 		game.FinishTheGame();
