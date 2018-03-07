d4ae0b27765854809f80c6c6b6c38995ec7db247
Edu <36489953+EdwardMartinz@users.noreply.github.com>
2018-03-07 23:29:19 +0100

    Update ResetCommand.java

--- src/commands/ResetCommand.java
+++ src/commands/ResetCommand.java
@@ -10,12 +10,10 @@ public class ResetCommand extends NoParamsCommand {
 	private final static String COMMAND_INFO = "reset";
 	private final static String HELP_INFO = "start a new game\n";
 
-	// Constructor
 	public ResetCommand() {
 		super(COMMAND_INFO, HELP_INFO);
 	}
-
-	// Execute
+	
 	@Override
 	public boolean execute(Game game) {
 		long newSeed = new Random().nextInt(1000); // creates a new seed
