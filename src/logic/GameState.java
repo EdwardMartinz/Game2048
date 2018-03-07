9184c5be4ef3e8cc2c5773b7601ee3ea5953a52e
Edu <36489953+EdwardMartinz@users.noreply.github.com>
2018-03-07 23:39:16 +0100

    Update GameState.java

--- src/logic/GameState.java
+++ src/logic/GameState.java
@@ -2,17 +2,14 @@ package logic;
 
 public class GameState {
 
-	// Attributes
 	private int score;
 	private int[][] boardState;
 
-	// Constructor
 	public GameState(int score, int[][] boardState) {
 		this.score = score;
 		this.boardState = boardState;
 	}
 
-	// Getters
 	public int getScore() {
 		return score;
 	}
